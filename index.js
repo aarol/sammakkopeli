612fbe323eb69326b64bc130c19a8890cc909147
Aaro Luomanen <aaro.luomanen@aalto.fi>
2025-01-26 18:02:08 +0200

    Background

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ class Game {
       width: 800,
       height: 600,
       pixelArt: true,
+      audio: {noAudio: true},
       physics: {
         default: "arcade",
         arcade: {
           gravity: { y: 450, x: 0 },
-          debug: true,
+          debug: false,
         },
       },
       scene: MainScene,
@@ -24,6 +25,7 @@ class MainScene extends Phaser.Scene {
     super("MainScene")
   }
   preload() {
+    this.load.image("background", "assets/scifi_bg.jpg")
     this.load.image("sky", "assets/sky.png");
     this.load.image("ground", "assets/platform.png");
     this.load.image("wall", "assets/platform.png")
@@ -46,6 +48,10 @@ class MainScene extends Phaser.Scene {
   create() {
     //  A simple background for our game
     this.add.image(400, 300, "sky");
+    this.background = this.add.tileSprite(0, 0, 0, 0, "background").setOrigin(0, 0)
+    this.bgAspectRatio = this.background.width / this.background.height
+    this.background.displayHeight = this.sys.canvas.height
+    this.background.displayWidth = this.sys.canvas.height * (this.bgAspectRatio)
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     this.platforms = this.physics.add.staticGroup();
@@ -54,8 +60,9 @@ class MainScene extends Phaser.Scene {
     //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
     this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
 
-    this.player = this.physics.add.sprite(100, 300, "frog");
-    this.player.setScale(2);
+    this.player = this.physics.add.sprite(100, 450, "frog");
+    this.player.setScale(4);
+    
 
     //  Our player animations, turning, walking left and walking right.
     this.anims.create({
@@ -92,7 +99,7 @@ class MainScene extends Phaser.Scene {
       key: "wall",
       repeat: this.numberOfObstacles,
       allowGravity: false,
-      setXY: { x: 300, y: 650, stepX: this.distanceBetweenObstacles },
+      setXY: { x: 550, y: 650, stepX: this.distanceBetweenObstacles },
       setRotation: { value: Math.PI / 2 },
     })
 
@@ -100,7 +107,7 @@ class MainScene extends Phaser.Scene {
       // flip width and height to align with collision box
       // better to have an actual horizontal obstacle texture
       // @ts-ignore
-      wall.body.setSize(32,400)
+      wall.body.setSize(32, 400)
       return true
     })
 
@@ -118,7 +125,7 @@ class MainScene extends Phaser.Scene {
     this.physics.add.collider(this.player, this.platforms);
 
     //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    //this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
 
     this.physics.add.collider(this.player, this.walls, this.hitWall, null, this);
   }
@@ -127,18 +134,19 @@ class MainScene extends Phaser.Scene {
     if (this.player.body.touching.down) {
       this.player.anims.play("walk", true)
       this.falling = false
-    }
 
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-400);
-      this.player.anims.play("jump", true);
-    } else if (!this.player.body.touching.down) {
-      if (this.player.body.velocity.y > 0) {
-        if (!this.falling) {
-          this.player.anims.stop()
-          this.player.play("fall", true);
-          this.falling = true
-        }
+      if (this.cursors.up.isDown || this.input.activePointer.isDown) {
+        this.player.setVelocityY(-400)
+        this.player.anims.play("jump", true);
+      }
+    } else {
+      // in air
+      if (this.player.body.velocity.y > 0 && !this.falling) {
+        // 🤓 uhmm.. actually we are falling
+        this.player.anims.stop()
+        this.player.play("fall", true);
+        // set falling flag to not play animation repeatedly
+        this.falling = true
       }
     }
   }
@@ -148,7 +156,7 @@ class MainScene extends Phaser.Scene {
       star.body.position.x -= delta * this.obstacleSpeedMultiplier
 
       if (star.body.position.x < -100) {
-        star.body.position.x += this.numberOfObstacles*this.distanceBetweenObstacles
+        star.body.position.x += this.numberOfObstacles * this.distanceBetweenObstacles
       }
 
       return true
@@ -158,12 +166,13 @@ class MainScene extends Phaser.Scene {
       wall.body.position.x -= delta * this.obstacleSpeedMultiplier
 
       if (wall.body.position.x < -300) {
-        wall.body.position.x += this.numberOfObstacles*this.distanceBetweenObstacles
+        wall.body.position.x += this.numberOfObstacles * this.distanceBetweenObstacles
       }
-
 
       return true
     })
+
+    this.background.tilePositionX += 1
   }
 
 
@@ -176,24 +185,20 @@ class MainScene extends Phaser.Scene {
   }
 
   hitWall(player, wall) {
-    return
     this.physics.pause()
-    console.log("hit wall")
+    this.player.anims.stop()
 
     player.setTint(0xff0000)
 
     this.gameOver = true
   }
+
+  collectStar(player, star) {
+    star.disableBody(true, true)
+
+    this.score += 10;
+    this.scoreText.setText('Score: ' + this.score);
+  }
 }
-
-// function hitBomb(player, bomb) {
-//   this.physics.pause();
-
-//   player.setTint(0xff0000);
-
-//   player.anims.play("turn", true);
-
-//   gameOver = true;
-// }
 
 new Game()
